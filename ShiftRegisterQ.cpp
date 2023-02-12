ba80e6879e2e72eab65480b111e9991186d8c4a5
markbowhill <markbowhill@gmail.com>
2023-02-12 01:59:27 -0400

    Agregado Tipo de pantalla

--- ShiftRegisterQ.cpp
+++ ShiftRegisterQ.cpp
@@ -1,25 +1,28 @@
 #include "ShiftRegisterQ.h"
 
 
-ShiftRegisterQ::ShiftRegisterQ(uint8_t pinD, uint8_t pinL, uint8_t pinC)
+ShiftRegisterQ::ShiftRegisterQ()
 {
-    setPins(pinD, pinL, pinC);
-    _orderBit = true;
-    _displaysQuantity = 1;
+    setPins(7, 9, 8);
+    setDisplay(7, 1);
 }
 
-ShiftRegisterQ::ShiftRegisterQ(uint8_t pinD, uint8_t pinL, uint8_t pinC, bool bitO)
+ShiftRegisterQ::ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL)
 {
-    setPins(pinD, pinL, pinC);
-    _orderBit = bitO;
-    _displaysQuantity = 1;
+    setPins(pinD, pinC, pinL);
+    setDisplay(7, 1);
 }
 
-ShiftRegisterQ::ShiftRegisterQ(uint8_t pinD, uint8_t pinL, uint8_t pinC, bool bitO, uint8_t disQ)
+ShiftRegisterQ::ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL, uint8_t disT)
 {
-    setPins(pinD, pinL, pinC);
-    _orderBit = bitO;
-    _displaysQuantity = disQ;
+    setPins(pinD, pinC, pinL);
+    setDisplay(disT, 1);
+}
+
+ShiftRegisterQ::ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL, uint8_t disT, uint16_t disQ)
+{
+    setPins(pinD, pinC, pinL);
+    setDisplay(disT, disQ);
 }
 
 void ShiftRegisterQ::print(String * datos){
@@ -37,7 +40,7 @@ void ShiftRegisterQ::print(String * datos){
             shiftOut(_pinData, _pinClock, _orderBit, 0);
         }
     }
-    uint8_t byteData;
+    uint16_t byteData;
     for(uint8_t i = 0; i < largoDatos; i++){
         char d = datos->charAt(i);
         if(d != '.' && d != ','){
@@ -45,7 +48,7 @@ void ShiftRegisterQ::print(String * datos){
             if(ds != '.' && ds != ','){
                 byteData = transChar(d);
             }else{
-                byteData = transChar(d)+128;
+                byteData = transChar(d)+transChar('.');
             }
             shiftOut(_pinData, _pinClock, _orderBit, byteData);
         }
@@ -54,86 +57,101 @@ void ShiftRegisterQ::print(String * datos){
     digitalWrite(_pinLatch, LOW);
 }
 
-void ShiftRegisterQ::setPins(uint8_t pinD, uint8_t pinL, uint8_t pinC){
+void ShiftRegisterQ::setPins(uint8_t pinD, uint8_t pinC, uint8_t pinL){
     _pinData    = pinD;
-    _pinLatch   = pinL;
     _pinClock   = pinC;
+    _pinLatch   = pinL;
     pinMode(_pinData, OUTPUT);
-    pinMode(_pinLatch, OUTPUT);
     pinMode(_pinClock, OUTPUT);
+    pinMode(_pinLatch, OUTPUT);
 }
 
-uint8_t ShiftRegisterQ::transChar(char ch){
-    uint8_t bits;
-    switch(ch){
-        case '0':  bits = 1 + 2 + 4 + 8 + 16 + 32;        break;
-        case '1':  bits = 2 + 4;                          break;
-        case '2':  bits = 1 + 2 + 8 + 16 + 64;            break;
-        case '3':  bits = 1 + 2 + 4 + 8 + 64;             break;
-        case '4':  bits = 2 + 4 + 32 + 64;                break;
-        case '5':  bits = 1 + 4 + 8 + 32 + 64;            break;
-        case '6':  bits = 1 + 4 + 8 + 16 + 32 + 64;       break;
-        case '7':  bits = 1 + 2 + 4;                      break;
-        case '8':  bits = 1 + 2 + 4 + 8 + 16 + 32 + 64;   break;
-        case '9':  bits = 1 + 2 + 4 + 8 + 32 + 64;        break;
-        case 'A':  bits = 1 + 2 + 4 + 16 + 32 + 64;       break;
-        case 'B':  bits = 4 + 8 + 16 + 32 + 64;           break;
-        case 'C':  bits = 1 + 8 + 16 + 32;                break;
-        case 'D':  bits = 2 + 4 + 8 + 16 + 64;            break;
-        case 'E':  bits = 1 + 8 + 16 + 32 + 64;           break;
-        case 'F':  bits = 1 + 16 + 32 + 64;               break;
-        //case 'G':  bits = 0;                              break;
-        case 'H':  bits = 2 + 4 + 16 + 32 + 64;           break;
-        case 'I':  bits = 16 + 32;                        break;
-        case 'J':  bits = 2 + 4 + 8 + 16;                 break;
-        //case 'K':  bits = 0;                              break;
-        case 'L':  bits = 8 + 16 + 32;                    break;
-        //case 'M':  bits = 0;                              break;
-        case 'N':  bits = 1 + 2 + 4 + 16 + 32;            break;
-        //case 'Ñ':  bits = 0;                              break;
-        case 'O':  bits = 1 + 2 + 4 + 8 + 16 + 32;        break;
-        case 'P':  bits = 1 + 2 + 16 + 32 + 64;           break;
-        //case 'Q':  bits = 0;                              break;
-        //case 'R':  bits = 0;                              break;
-        //case 'S':  bits = 0;                              break;
-        //case 'T':  bits = 0;                              break;
-        //case 'U':  bits = 0;                              break;
-        //case 'V':  bits = 0;                              break;
-        //case 'W':  bits = 0;                              break;
-        //case 'X':  bits = 0;                              break;
-        case 'Y':  bits = 2 + 4 + 8 + +32 + 64;           break;
-        //case 'Z':  bits = 0;                              break;
-        case 'a':  bits = 1 + 2 + 4 + 16 + 32 + 64;       break;
-        case 'b':  bits = 4 + 8 + 16 + 32 + 64;           break;
-        case 'c':  bits = 1 + 8 + 16 + 32;                break;
-        case 'd':  bits = 2 + 4 + 8 + 16 + 64;            break;
-        case 'e':  bits = 1 + 8 + 16 + 32 + 64;           break;
-        case 'f':  bits = 1 + 16 + 32 + 64;               break;
-        //case 'g':  bits = 0;                              break;
-        case 'h':  bits = 2 + 4 + 16 + 32 + 64;           break;
-        case 'i':  bits = 16 + 32;                        break;
-        case 'j':  bits = 2 + 4 + 8 + 16;                 break;
-        //case 'k':  bits = 0;                              break;
-        case 'l':  bits = 8 + 16 + 32;                    break;
-        //case 'm':  bits = 0;                              break;
-        case 'n':  bits = 1 + 2 + 4 + 16 + 32;            break;
-        //case 'ñ':  bits = 0;                              break;
-        case 'o':  bits = 1 + 2 + 4 + 8 + 16 + 32;        break;
-        case 'p':  bits = 1 + 2 + 16 + 32 + 64;           break;
-        //case 'q':  bits = 0;                              break;
-        //case 'r':  bits = 0;                              break;
-        //case 's':  bits = 0;                              break;
-        //case 't':  bits = 0;                              break;
-        //case 'u':  bits = 0;                              break;
-        //case 'v':  bits = 0;                              break;
-        //case 'w':  bits = 0;                              break;
-        //case 'x':  bits = 0;                              break;
-        case 'y':  bits = 2 + 4 + 8 + +32 + 64;           break;
-        //case 'z':  bits = 0;                              break;
-        case '-':  bits = 64;                             break;
-        case '_':  bits = 8;                              break;
-        case ' ':  bits = 0;                              break;
-        default:   bits = 0;
+void ShiftRegisterQ::setDisplay(uint8_t disT, uint16_t disQ){
+    _displayType        = disT;
+    _displaysQuantity   = disQ;
+}
+
+uint16_t ShiftRegisterQ::transChar(char ch){
+    uint16_t bits;
+    if(_displayType == 7){
+        switch(ch){
+            case '0':  bits = 1 + 2 + 4 + 8 + 16 + 32;        break;
+            case '1':  bits = 2 + 4;                          break;
+            case '2':  bits = 1 + 2 + 8 + 16 + 64;            break;
+            case '3':  bits = 1 + 2 + 4 + 8 + 64;             break;
+            case '4':  bits = 2 + 4 + 32 + 64;                break;
+            case '5':  bits = 1 + 4 + 8 + 32 + 64;            break;
+            case '6':  bits = 1 + 4 + 8 + 16 + 32 + 64;       break;
+            case '7':  bits = 1 + 2 + 4;                      break;
+            case '8':  bits = 1 + 2 + 4 + 8 + 16 + 32 + 64;   break;
+            case '9':  bits = 1 + 2 + 4 + 8 + 32 + 64;        break;
+            case 'A':  bits = 1 + 2 + 4 + 16 + 32 + 64;       break;
+            case 'B':  bits = 4 + 8 + 16 + 32 + 64;           break;
+            case 'C':  bits = 1 + 8 + 16 + 32;                break;
+            case 'D':  bits = 2 + 4 + 8 + 16 + 64;            break;
+            case 'E':  bits = 1 + 8 + 16 + 32 + 64;           break;
+            case 'F':  bits = 1 + 16 + 32 + 64;               break;
+            //case 'G':  bits = 0;                              break;
+            case 'H':  bits = 2 + 4 + 16 + 32 + 64;           break;
+            case 'I':  bits = 16 + 32;                        break;
+            case 'J':  bits = 2 + 4 + 8 + 16;                 break;
+            //case 'K':  bits = 0;                              break;
+            case 'L':  bits = 8 + 16 + 32;                    break;
+            //case 'M':  bits = 0;                              break;
+            case 'N':  bits = 1 + 2 + 4 + 16 + 32;            break;
+            //case 'Ñ':  bits = 0;                              break;
+            case 'O':  bits = 1 + 2 + 4 + 8 + 16 + 32;        break;
+            case 'P':  bits = 1 + 2 + 16 + 32 + 64;           break;
+            //case 'Q':  bits = 0;                              break;
+            //case 'R':  bits = 0;                              break;
+            //case 'S':  bits = 0;                              break;
+            //case 'T':  bits = 0;                              break;
+            //case 'U':  bits = 0;                              break;
+            //case 'V':  bits = 0;                              break;
+            //case 'W':  bits = 0;                              break;
+            //case 'X':  bits = 0;                              break;
+            case 'Y':  bits = 2 + 4 + 8 + +32 + 64;           break;
+            //case 'Z':  bits = 0;                              break;
+            case 'a':  bits = 1 + 2 + 4 + 16 + 32 + 64;       break;
+            case 'b':  bits = 4 + 8 + 16 + 32 + 64;           break;
+            case 'c':  bits = 1 + 8 + 16 + 32;                break;
+            case 'd':  bits = 2 + 4 + 8 + 16 + 64;            break;
+            case 'e':  bits = 1 + 8 + 16 + 32 + 64;           break;
+            case 'f':  bits = 1 + 16 + 32 + 64;               break;
+            //case 'g':  bits = 0;                              break;
+            case 'h':  bits = 2 + 4 + 16 + 32 + 64;           break;
+            case 'i':  bits = 16 + 32;                        break;
+            case 'j':  bits = 2 + 4 + 8 + 16;                 break;
+            //case 'k':  bits = 0;                              break;
+            case 'l':  bits = 8 + 16 + 32;                    break;
+            //case 'm':  bits = 0;                              break;
+            case 'n':  bits = 1 + 2 + 4 + 16 + 32;            break;
+            //case 'ñ':  bits = 0;                              break;
+            case 'o':  bits = 1 + 2 + 4 + 8 + 16 + 32;        break;
+            case 'p':  bits = 1 + 2 + 16 + 32 + 64;           break;
+            //case 'q':  bits = 0;                              break;
+            //case 'r':  bits = 0;                              break;
+            //case 's':  bits = 0;                              break;
+            //case 't':  bits = 0;                              break;
+            //case 'u':  bits = 0;                              break;
+            //case 'v':  bits = 0;                              break;
+            //case 'w':  bits = 0;                              break;
+            //case 'x':  bits = 0;                              break;
+            case 'y':  bits = 2 + 4 + 8 + +32 + 64;           break;
+            //case 'z':  bits = 0;                              break;
+            case '-':  bits = 64;                             break;
+            case '_':  bits = 8;                              break;
+            case ' ':  bits = 0;                              break;
+            case '.':  bits = 128;                            break;
+            case ',':  bits = 128;                            break;
+            default:   bits = 0;
+        }
+    }
+    if(_displayType == 15){
+
+    }
+    if(_displayType == 16){
+
     }
     return bits;
 }
--- ShiftRegisterQ.h
+++ ShiftRegisterQ.h
@@ -5,17 +5,20 @@
 
 class ShiftRegisterQ {
   public:
-    ShiftRegisterQ(uint8_t pinD, uint8_t pinL, uint8_t pinC);
-    ShiftRegisterQ(uint8_t pinD, uint8_t pinL, uint8_t pinC, bool bitO);
-    ShiftRegisterQ(uint8_t pinD, uint8_t pinL, uint8_t pinC, bool bitO, uint8_t pantQ);
+    ShiftRegister();
+    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL);
+    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL, uint8_t disT);
+    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL, uint8_t disT, uint16_t disQ);
     void print(String * datos);
-    void setPins(uint8_t pinD, uint8_t pinL, uint8_t pinC);
+    void setPins(uint8_t pinD, uint8_t pinC, uint8_t pinL);
+    void setDisplay(uint8_t disT, uint16_t disQ);
     
   private:
-    uint8_t   transChar(char ch);
-    uint8_t   _displaysQuantity;
+    uint16_t   transChar(char ch);
     uint8_t   _pinData;
-    uint8_t   _pinLatch;
     uint8_t   _pinClock;
-    bool      _orderBit;
+    uint8_t   _pinLatch;
+    uint8_t   _displayType;
+    uint16_t  _displaysQuantity;
+    bool      _orderBit = true; // default MSBFIRST
 };
