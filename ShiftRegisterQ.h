0d9a8cde8cde2c8dd4265c275a3dbbea70a91657
markbowhill <markbowhill@gmail.com>
2023-02-25 22:04:49 -0400

    retirados nombres de parametros en metodos del .h

--- ShiftRegisterQ.h
+++ ShiftRegisterQ.h
@@ -6,19 +6,19 @@
 class ShiftRegisterQ {
   public:
     ShiftRegisterQ();
-    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL);
-    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL, uint8_t disT);
-    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL, uint8_t disT, uint16_t disQ);
-    void print(String * datos);
-    void setPins(uint8_t pinD, uint8_t pinC, uint8_t pinL);
-    void setDisplay(uint8_t disT, uint16_t disQ);
+    ShiftRegisterQ(uint8_t, uint8_t, uint8_t);
+    ShiftRegisterQ(uint8_t, uint8_t, uint8_t, uint8_t);
+    ShiftRegisterQ(uint8_t, uint8_t, uint8_t, uint8_t, uint16_t);
+    void print(String *);
+    void setPins(uint8_t, uint8_t, uint8_t);
+    void setDisplay(uint8_t, uint16_t);
     void setDelayScroll(uint32_t);
     void clearAll();
     
   private:
     void      sendOne(char, bool);
     void      run();
-    uint16_t  transChar(char ch);
+    uint16_t  transChar(char);
     uint8_t   _pinData;
     uint8_t   _pinClock;
     uint8_t   _pinLatch;
