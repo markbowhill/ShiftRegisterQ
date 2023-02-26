8fb651049fc1f3d4cff3721d008a423eab186e77
markbowhill <markbowhill@gmail.com>
2023-02-25 22:07:29 -0400

    reajuste en constructores

--- ShiftRegisterQ.h
+++ ShiftRegisterQ.h
@@ -6,10 +6,10 @@
 class ShiftRegisterQ {
   public:
     ShiftRegisterQ();
-    ShiftRegisterQ(uint8_t, uint8_t, uint8_t);
-    ShiftRegisterQ(uint8_t, uint8_t, uint8_t, uint8_t);
-    ShiftRegisterQ(uint8_t, uint8_t, uint8_t, uint8_t, uint16_t);
-    void print(String *);
+    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL);
+    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL, uint8_t disT);
+    ShiftRegisterQ(uint8_t pinD, uint8_t pinC, uint8_t pinL, uint8_t disT, uint16_t disQ);
+    void print(String*);
     void setPins(uint8_t, uint8_t, uint8_t);
     void setDisplay(uint8_t, uint16_t);
     void setDelayScroll(uint32_t);
@@ -25,5 +25,5 @@ class ShiftRegisterQ {
     uint8_t   _displayType;
     uint16_t  _displaysQuantity;
     bool      _orderBit = true; // default MSBFIRST
-    uint32_t  _delayScroll = 200;
+    uint32_t  _delayScroll = 500;
 };
